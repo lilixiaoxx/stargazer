ef0adf491800ec9c26b597fbb8c3f314a94932d8
xi chen <chenxigirl@gmail.com>
2016-07-11 19:01:24 -0500

    hw done

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,28 @@
+$(document).ready(function(){
+  $('#submit').click(function(event){
+    event.preventDefault();
+    var name=$('#search-name').val();
+    search(name);
+  })
+})
+
 function search(name) {
   var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
   // requires "jsonp" datatype.
-  
+  // debugger
+  $.ajax({
+    method: "GET",
+    url: api,
+    dataType: "jsonp",
+    jsonpCallback: "showimage"
+  });
 }
+
+function showimage(json){
+  // console.log("hello")
+  // debugger
+  var url = json.image.src;
+  $('#preview').html(`<div><img src="${url}"/></div>`);
+}
+
+
